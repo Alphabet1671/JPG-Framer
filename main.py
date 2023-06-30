e4ac694286a55aba1775b44a52a6e1d1b95ce2a1
Alphabet1671 <Alphabet1671@foxmail.com>
2023-06-30 17:05:31 +0800

    Integrated preview into main window and massively increased preview speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
 import exifread
@@ -9,16 +11,18 @@ minBoarderWidth = 0.1
 minBoarderHeight = 0.2
 
 
-def AddedSquareBoarder(sourceLocation, backgroundColor, textColor, textSize, finalImageRatio, displayTopLeft, displayTopRight, displayBottomLeft, displayBottomRight):
+def AddedSquareBoarder(sourceLocation, backgroundColor, textColor, textSize, fontPath ,finalImageRatio, minBoarderWidth, minBoarderHeight, displayTopLeft, displayTopRight, displayBottomLeft, displayBottomRight):
     sourceImage = Image.open(sourceLocation)
     sourceWidth = sourceImage.size[0]
     sourceHeight = sourceImage.size[1]
     if (sourceWidth/finalImageRatio)>sourceHeight:
         resultWidth = int(sourceWidth*(1+minBoarderWidth))
         resultHeight = int(resultWidth/finalImageRatio)
+        textHeight = int(resultWidth-sourceWidth)*(minBoarderHeight/minBoarderWidth)/2*textSize
     else:
         resultHeight = int(sourceHeight*(1+minBoarderHeight))
         resultWidth = int(resultHeight*finalImageRatio)
+        textHeight =  int(resultHeight-sourceHeight)/2*textSize
     resultImage = Image.new("RGB", (resultWidth, resultHeight), backgroundColor)
     resultImage.paste(sourceImage, (int((resultWidth-sourceWidth)/2),int((resultHeight-sourceHeight)/2)))
 
@@ -29,7 +33,6 @@ def AddedSquareBoarder(sourceLocation, backgroundColor, textColor, textSize, fin
     f = open(sourceLocation, "rb")
 
     tags = exifread.process_file(f)
-    print(str(tags))
 
     """
     shutterSpeedString = str(tags["EXIF ExposureTime"])
@@ -81,10 +84,12 @@ def AddedSquareBoarder(sourceLocation, backgroundColor, textColor, textSize, fin
 
     paddingHeight = int(resultHeight-sourceHeight)/2
     paddingWidth = int(resultWidth-sourceWidth)/2
-    textHeight = paddingHeight*textSize
 
-    cameraSettingFont = ImageFont.truetype("calibri-italic.ttf", int(textHeight))
-    cameraInfoFont = ImageFont.truetype("calibri-italic.ttf", int(textHeight))
+
+
+    cameraSettingFont = ImageFont.truetype(fontPath, int(textHeight))
+    cameraInfoFont = ImageFont.truetype(fontPath, int(textHeight))
+
 
 
     topLeftTextOffset = (paddingWidth,paddingHeight/2)
@@ -127,6 +132,11 @@ class MainWindow(QMainWindow):
         self.displayTopRight = False
         self.displayBottomLeft = False
         self.displayBottomRight = False
+
+        self.fontFileDirectory = "calibri-italic.ttf"
+
+        self.minBoarderWidth = 0.1
+        self.minBoarderHeight = 0.2
 
 #Browse button for directory input
         inputLayout = QVBoxLayout()
@@ -174,7 +184,7 @@ class MainWindow(QMainWindow):
         toggleCameraInfoCheckBox = QCheckBox()
         toggleCameraInfoCheckBox.setCheckable(True)
         toggleCameraInfoCheckBox.setText("Show Camera Info")
-        toggleFocalLengthCheckBox.stateChanged.connect(self.SetDisplayBottomRight)
+        toggleCameraInfoCheckBox.stateChanged.connect(self.SetDisplayBottomRight)
 
 
         toggleLayout.addWidget(toggleSettingsCheckBox,0,0)
@@ -184,6 +194,7 @@ class MainWindow(QMainWindow):
 #==================================================================Set Background Color
 
         backgroundColorInputLayout = QHBoxLayout()
+        backgroundColorInputLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
         backgroundColorRInput = QDoubleSpinBox()
         backgroundColorRInput.setMaximum(255)
@@ -215,7 +226,7 @@ class MainWindow(QMainWindow):
         backgroundColorInputLayout.addWidget(backgroundColorBInput)
 #=================================================================
         textColorInputLayout = QHBoxLayout()
-
+        textColorInputLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
         textColorRInput = QDoubleSpinBox()
         textColorRInput.setMaximum(255)
         textColorRInput.setMinimum(0)
@@ -248,6 +259,7 @@ class MainWindow(QMainWindow):
 #==================================================================================Set Image Ratio
 
         ratioInputLayout = QHBoxLayout()
+        ratioInputLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
         widthRatioInput = QDoubleSpinBox()
         widthRatioInput.setDecimals(0)
@@ -269,32 +281,78 @@ class MainWindow(QMainWindow):
 #==========================================================================Text Size Input
 
         textSizeInputLayout = QHBoxLayout()
+        textSizeInputLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
         textSizeInput = QDoubleSpinBox()
-        textSizeInput.setDecimals(1)
+        textSizeInput.setDecimals(2)
         textSizeInput.setMaximum(1)
         textSizeInput.setMinimum(0)
-        textSizeInput.setSingleStep(0.1)
+        textSizeInput.setSingleStep(0.01)
         textSizeInput.setValue(self.textSize)
         textSizeInput.valueChanged.connect(self.SetTextSize)
 
         textSizeInputLayout.addWidget(QLabel("Text Height (Relative):"))
         textSizeInputLayout.addWidget(textSizeInput)
 
+#=======================================================================font file browse
+
+        fontDirectoryInputLayout = QHBoxLayout()
+
+        fontDirectoryBrowseButton = QPushButton("Browse")
+        fontDirectoryBrowseButton.clicked.connect(self.GetFontDirectory)
+        self.fontDirectoryLabel = QLabel()
+
+        fontDirectoryInputLayout.addWidget(QLabel("Font File:"))
+        fontDirectoryInputLayout.addWidget(self.fontDirectoryLabel)
+        fontDirectoryInputLayout.addWidget(fontDirectoryBrowseButton)
+
+
+#=======================================================================Image Boarder control
+
+        imageBoarderInputLayout = QHBoxLayout()
+
+        imageBoarderHeightInput = QDoubleSpinBox()
+        imageBoarderHeightInput.setMinimum(0)
+        imageBoarderHeightInput.setMaximum(1)
+        imageBoarderHeightInput.setSingleStep(0.01)
+        imageBoarderHeightInput.setValue(self.minBoarderHeight)
+        imageBoarderHeightInput.valueChanged.connect(self.SetMinBoarderHeight)
+
+        imageBoarderWidthInput = QDoubleSpinBox()
+        imageBoarderWidthInput.setMinimum(0)
+        imageBoarderWidthInput.setMaximum(1)
+        imageBoarderWidthInput.setSingleStep(0.01)
+        imageBoarderWidthInput.setValue(self.minBoarderWidth)
+        imageBoarderWidthInput.valueChanged.connect(self.SetMinBoarderWidth)
+
+        imageBoarderInputLayout.addWidget(QLabel("Minimum Frame Width (Relative to Source):"))
+        imageBoarderInputLayout.addWidget(imageBoarderWidthInput)
+        imageBoarderInputLayout.addWidget(QLabel("Minimum Frame Height (Relative to Source):"))
+        imageBoarderInputLayout.addWidget(imageBoarderHeightInput)
+#===============
+        previewLayout = QHBoxLayout()
+        previewLayout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.previewImageLabel1 = QLabel()
+        self.previewImageLabel2 = QLabel()
+        self.previewImageLabel1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        previewLayout.addWidget(self.previewImageLabel1)
+        previewLayout.addWidget(self.previewImageLabel2)
+
+
 #=======================================================================
-
-
-
         inputLayout.addLayout(directoryInputLayout)
         inputLayout.addLayout(ratioInputLayout)
+        inputLayout.addLayout(imageBoarderInputLayout)
         inputLayout.addWidget(QLabel("Display Elements:"))
         inputLayout.addLayout(toggleLayout)
         inputLayout.addLayout(backgroundColorInputLayout)
         inputLayout.addLayout(textSizeInputLayout)
         inputLayout.addLayout(textColorInputLayout)
+        inputLayout.addLayout(fontDirectoryInputLayout)
         inputLayout.addWidget(previewButton)
         inputLayout.addWidget(startButton)
-
+        inputLayout.addLayout(previewLayout)
 
         widget = QWidget()
         widget.setLayout(inputLayout)
@@ -315,12 +373,26 @@ class MainWindow(QMainWindow):
             self.targetDirectory = str(Path(dir))
             self.targetDirectoryLabel.setText(str(Path(dir)))
 
+    def GetFontDirectory(self):
+        fontFileDialog = QFileDialog()
+        fontFileDialog.setDefaultSuffix("ttf")
+        fontFileDialog.setNameFilter("*.ttf")
+        dir = fontFileDialog.getOpenFileName(self, "Select Custom Font File (ttf)")[0]
+        try:
+            if dir.split(".")[1]=="ttf":
+                self.fontFileDirectory = str(Path(dir))
+                self.fontDirectoryLabel.setText(self.fontFileDirectory)
+        except:
+            self.fontFileDirectory = "calibri-italic.ttf"
+
     def Preview(self):
-        AddedSquareBoarder("DSC_0647.jpg",(self.backgroundColorRValue,self.backgroundColorGValue,self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue),self.textSize, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).show("Sample 1")
-        AddedSquareBoarder("DSC_9871.jpg",(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue), self.textSize, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).show("Sample 2")
+        AddedSquareBoarder("DSC_0647.jpg",(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue), self.textSize, self.fontFileDirectory, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.minBoarderWidth,self.minBoarderHeight, self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).resize((int(350*self.imageWidthRatioValue),int(350*self.imageHeightRatioValue))).save("temp1.jpg")
+        AddedSquareBoarder("DSC_9871.jpg",(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue), self.textSize, self.fontFileDirectory, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.minBoarderWidth,self.minBoarderHeight, self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).resize((int(350*self.imageWidthRatioValue),int(350*self.imageHeightRatioValue))).save("temp2.jpg")
+        self.previewImageLabel1.setPixmap(QPixmap("temp1.jpg"))
+        self.previewImageLabel2.setPixmap(QPixmap("temp2.jpg"))
     def Start(self):
         for image in os.listdir(self.sourceDirectory):
-            AddedSquareBoarder(str(Path(self.sourceDirectory)/Path(image)),(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue),self.textSize,(self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).save(str(Path(self.targetDirectory)/Path("framed"+image)))
+            AddedSquareBoarder(str(Path(self.sourceDirectory)/Path(image)),(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue),self.textSize, self.fontFileDirectory, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.minBoarderWidth,self.minBoarderHeight, self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).save(str(Path(self.targetDirectory)/Path("framed"+image)))
 
     def SetDisplayTopLeft(self, checked):
         self.displayTopLeft = checked
@@ -348,6 +420,13 @@ class MainWindow(QMainWindow):
         self.imageHeightRatioValue = value
     def SetTextSize(self, value):
         self.textSize = value
+    def SetMinBoarderHeight(self, value):
+        self.minBoarderHeight = value
+    def SetMinBoarderWidth(self, value):
+        self.minBoarderWidth = value
+
+
+
 app = QApplication([])
 
 # Create a Qt widget, which will be our window.
