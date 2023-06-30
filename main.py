d662941994a638baada5878a14669a2eb23f7554
Alphabet1671 <Alphabet1671@foxmail.com>
2023-06-30 13:32:29 +0800

    Added a bunch of customization settings

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from PyQt6.QtWidgets import *
-from PyQt6.QtCore import Qt
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
 import exifread
@@ -10,18 +9,18 @@ minBoarderWidth = 0.1
 minBoarderHeight = 0.2
 
 
-def AddedSquareBoarder(sourceLocation, color):
+def AddedSquareBoarder(sourceLocation, backgroundColor, textColor, textSize, finalImageRatio, displayTopLeft, displayTopRight, displayBottomLeft, displayBottomRight):
     sourceImage = Image.open(sourceLocation)
     sourceWidth = sourceImage.size[0]
     sourceHeight = sourceImage.size[1]
-    if sourceWidth>sourceHeight:
+    if (sourceWidth/finalImageRatio)>sourceHeight:
         resultWidth = int(sourceWidth*(1+minBoarderWidth))
-        textHeight = (resultWidth-sourceWidth)*0.25
+        resultHeight = int(resultWidth/finalImageRatio)
     else:
-        resultWidth = int(sourceHeight*(1+minBoarderHeight))
-        textHeight = int(int(resultWidth-sourceHeight)*0.1)
-    resultImage = Image.new("RGB", (resultWidth, resultWidth), color)
-    resultImage.paste(sourceImage, (int((resultWidth-sourceWidth)/2),int((resultWidth-sourceHeight)/2)))
+        resultHeight = int(sourceHeight*(1+minBoarderHeight))
+        resultWidth = int(resultHeight*finalImageRatio)
+    resultImage = Image.new("RGB", (resultWidth, resultHeight), backgroundColor)
+    resultImage.paste(sourceImage, (int((resultWidth-sourceWidth)/2),int((resultHeight-sourceHeight)/2)))
 
 
 
@@ -80,9 +79,9 @@ def AddedSquareBoarder(sourceLocation, color):
 
 
 
-    paddingHeight = int(resultWidth-sourceHeight)/2
+    paddingHeight = int(resultHeight-sourceHeight)/2
     paddingWidth = int(resultWidth-sourceWidth)/2
-
+    textHeight = paddingHeight*textSize
 
     cameraSettingFont = ImageFont.truetype("calibri-italic.ttf", int(textHeight))
     cameraInfoFont = ImageFont.truetype("calibri-italic.ttf", int(textHeight))
@@ -90,12 +89,12 @@ def AddedSquareBoarder(sourceLocation, color):
 
     topLeftTextOffset = (paddingWidth,paddingHeight/2)
     topRightTextOffset = (resultWidth-paddingWidth,paddingHeight/2)
-    bottomLeftTextOffset = (paddingWidth,resultWidth-paddingHeight/2)
-    bottomRightTextOffset = (resultWidth-paddingWidth,resultWidth-paddingHeight/2)
-    draw.text(topLeftTextOffset, cameraSettingString, font=cameraSettingFont, fill=(0,0,0,255), anchor="lm")
-    draw.text(topRightTextOffset, exposureProgramString, font=cameraSettingFont, fill=(0,0,0,255), anchor="rm")
-    draw.text(bottomLeftTextOffset, focalLengthString, font=cameraSettingFont, fill=(0,0,0,255), anchor="lm")
-    draw.multiline_text(bottomRightTextOffset, cameraNameString+"\n"+lensNameString, font=cameraInfoFont, fill=(0,0,0,255),align="right", anchor="rm",spacing=int(textHeight*0.1))
+    bottomLeftTextOffset = (paddingWidth,resultHeight-paddingHeight/2)
+    bottomRightTextOffset = (resultWidth-paddingWidth,resultHeight-paddingHeight/2)
+    if displayTopLeft: draw.text(topLeftTextOffset, cameraSettingString, font=cameraSettingFont, fill=textColor, anchor="lm")
+    if displayTopRight: draw.text(topRightTextOffset, exposureProgramString, font=cameraSettingFont, fill=textColor, anchor="rm")
+    if displayBottomLeft: draw.text(bottomLeftTextOffset, focalLengthString, font=cameraSettingFont, fill=textColor, anchor="lm")
+    if displayBottomRight: draw.multiline_text(bottomRightTextOffset, cameraNameString+"\n"+lensNameString, font=cameraInfoFont, fill=textColor,align="right", anchor="rm",spacing=int(textHeight*0.1))
     return resultImage
 #AddedSquareBoarder("Test/DSC_0647.jpg",(255,255,255)).show()
 #AddedSquareBoarder("Test/DSC_9871.jpg",(255,255,255)).show()
@@ -110,12 +109,28 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Widgets App")
         self.sourceDirectory = ""
         self.targetDirectory = ""
+
         self.backgroundColorRValue = 255
         self.backgroundColorGValue = 255
         self.backgroundColorBValue = 255
 
-#Browse button for directory input
+        self.textColorRValue = 0
+        self.textColorGValue = 0
+        self.textColorBValue = 0
 
+        self.textSize = 0.2
+
+        self.imageWidthRatioValue = 1
+        self.imageHeightRatioValue = 1
+
+        self.displayTopLeft = False
+        self.displayTopRight = False
+        self.displayBottomLeft = False
+        self.displayBottomRight = False
+
+#Browse button for directory input
+        inputLayout = QVBoxLayout()
+#============================================================================Directory Input
         directoryInputLayout = QGridLayout()
 
         sourceDirectoryBrowseButton = QPushButton("Browse")
@@ -129,14 +144,154 @@ class MainWindow(QMainWindow):
         directoryInputLayout.addWidget(sourceDirectoryBrowseButton,0,1)
         directoryInputLayout.addWidget(self.targetDirectoryLabel,1,0)
         directoryInputLayout.addWidget(targetDirectoryBrowseButton,1,1)
-
+#===============================================================================Preview & Start Buttons
         previewButton = QPushButton("Preview")
         previewButton.clicked.connect(self.Preview)
         startButton = QPushButton("Convert")
         startButton.released.connect(self.Start)
-        inputLayout = QVBoxLayout()
+
+
+#=================================================== Toggle Display Elements
+        toggleLayout = QGridLayout()
+
+        toggleSettingsCheckBox = QCheckBox()
+        toggleSettingsCheckBox.setCheckable(True)
+        toggleSettingsCheckBox.setText("Show Camera Settings")
+        toggleSettingsCheckBox.stateChanged.connect(self.SetDisplayTopLeft)
+        #remember to connect the checkboxes
+
+        toggleProgramCheckBox = QCheckBox()
+        toggleProgramCheckBox.setCheckable(True)
+        toggleProgramCheckBox.setText("Show Exposure Program")
+        toggleProgramCheckBox.stateChanged.connect(self.SetDisplayTopRight)
+
+        toggleFocalLengthCheckBox = QCheckBox()
+        toggleFocalLengthCheckBox.setCheckable(True)
+        toggleFocalLengthCheckBox.setText("Show Focal Length")
+        toggleFocalLengthCheckBox.stateChanged.connect(self.SetDisplayBottomLeft)
+
+
+        toggleCameraInfoCheckBox = QCheckBox()
+        toggleCameraInfoCheckBox.setCheckable(True)
+        toggleCameraInfoCheckBox.setText("Show Camera Info")
+        toggleFocalLengthCheckBox.stateChanged.connect(self.SetDisplayBottomRight)
+
+
+        toggleLayout.addWidget(toggleSettingsCheckBox,0,0)
+        toggleLayout.addWidget(toggleProgramCheckBox,0,1)
+        toggleLayout.addWidget(toggleFocalLengthCheckBox,1,0)
+        toggleLayout.addWidget(toggleCameraInfoCheckBox,1,1)
+#==================================================================Set Background Color
+
+        backgroundColorInputLayout = QHBoxLayout()
+
+        backgroundColorRInput = QDoubleSpinBox()
+        backgroundColorRInput.setMaximum(255)
+        backgroundColorRInput.setMinimum(0)
+        backgroundColorRInput.setDecimals(0)
+        backgroundColorRInput.setValue(self.backgroundColorRValue)
+        backgroundColorRInput.valueChanged.connect(self.SetBackgroundColorRValue)
+
+        backgroundColorGInput = QDoubleSpinBox()
+        backgroundColorGInput.setMaximum(255)
+        backgroundColorGInput.setMinimum(0)
+        backgroundColorGInput.setDecimals(0)
+        backgroundColorGInput.setValue(self.backgroundColorGValue)
+        backgroundColorGInput.valueChanged.connect(self.SetBackgroundColorGValue)
+
+        backgroundColorBInput = QDoubleSpinBox()
+        backgroundColorBInput.setMaximum(255)
+        backgroundColorBInput.setMinimum(0)
+        backgroundColorBInput.setDecimals(0)
+        backgroundColorBInput.setValue(self.backgroundColorBValue)
+        backgroundColorBInput.valueChanged.connect(self.SetBackgroundColorBValue)
+
+        backgroundColorInputLayout.addWidget(QLabel("Background Color: "))
+        backgroundColorInputLayout.addWidget(QLabel("R:"))
+        backgroundColorInputLayout.addWidget(backgroundColorRInput)
+        backgroundColorInputLayout.addWidget(QLabel("G:"))
+        backgroundColorInputLayout.addWidget(backgroundColorGInput)
+        backgroundColorInputLayout.addWidget(QLabel("B:"))
+        backgroundColorInputLayout.addWidget(backgroundColorBInput)
+#=================================================================
+        textColorInputLayout = QHBoxLayout()
+
+        textColorRInput = QDoubleSpinBox()
+        textColorRInput.setMaximum(255)
+        textColorRInput.setMinimum(0)
+        textColorRInput.setDecimals(0)
+        textColorRInput.setValue(self.textColorRValue)
+        textColorRInput.valueChanged.connect(self.SetTextColorRValue)
+
+        textColorGInput = QDoubleSpinBox()
+        textColorGInput.setMaximum(255)
+        textColorGInput.setMinimum(0)
+        textColorGInput.setDecimals(0)
+        textColorGInput.setValue(self.textColorGValue)
+        textColorGInput.valueChanged.connect(self.SetTextColorGValue)
+
+        textColorBInput = QDoubleSpinBox()
+        textColorBInput.setMaximum(255)
+        textColorBInput.setMinimum(0)
+        textColorBInput.setDecimals(0)
+        textColorBInput.setValue(self.textColorBValue)
+        textColorBInput.valueChanged.connect(self.SetTextColorBValue)
+
+        textColorInputLayout.addWidget(QLabel("Text Color: "))
+        textColorInputLayout.addWidget(QLabel("R:"))
+        textColorInputLayout.addWidget(textColorRInput)
+        textColorInputLayout.addWidget(QLabel("G:"))
+        textColorInputLayout.addWidget(textColorGInput)
+        textColorInputLayout.addWidget(QLabel("B:"))
+        textColorInputLayout.addWidget(textColorBInput)
+
+#==================================================================================Set Image Ratio
+
+        ratioInputLayout = QHBoxLayout()
+
+        widthRatioInput = QDoubleSpinBox()
+        widthRatioInput.setDecimals(0)
+        widthRatioInput.setMinimum(0)
+        widthRatioInput.setValue(self.imageWidthRatioValue)
+        widthRatioInput.valueChanged.connect(self.SetImageWidthRatioValue)
+
+        heightRatioInput = QDoubleSpinBox()
+        heightRatioInput.setDecimals(0)
+        heightRatioInput.setMinimum(0)
+        heightRatioInput.setValue(self.imageHeightRatioValue)
+        heightRatioInput.valueChanged.connect(self.SetImageHeightRatioValue)
+
+
+        ratioInputLayout.addWidget(QLabel("Set Final Image Ratio (Width:Height)"))
+        ratioInputLayout.addWidget(widthRatioInput)
+        ratioInputLayout.addWidget(QLabel(":"))
+        ratioInputLayout.addWidget(heightRatioInput)
+#==========================================================================Text Size Input
+
+        textSizeInputLayout = QHBoxLayout()
+
+        textSizeInput = QDoubleSpinBox()
+        textSizeInput.setDecimals(1)
+        textSizeInput.setMaximum(1)
+        textSizeInput.setMinimum(0)
+        textSizeInput.setSingleStep(0.1)
+        textSizeInput.setValue(self.textSize)
+        textSizeInput.valueChanged.connect(self.SetTextSize)
+
+        textSizeInputLayout.addWidget(QLabel("Text Height (Relative):"))
+        textSizeInputLayout.addWidget(textSizeInput)
+
+#=======================================================================
+
+
 
         inputLayout.addLayout(directoryInputLayout)
+        inputLayout.addLayout(ratioInputLayout)
+        inputLayout.addWidget(QLabel("Display Elements:"))
+        inputLayout.addLayout(toggleLayout)
+        inputLayout.addLayout(backgroundColorInputLayout)
+        inputLayout.addLayout(textSizeInputLayout)
+        inputLayout.addLayout(textColorInputLayout)
         inputLayout.addWidget(previewButton)
         inputLayout.addWidget(startButton)
 
@@ -161,16 +316,43 @@ class MainWindow(QMainWindow):
             self.targetDirectoryLabel.setText(str(Path(dir)))
 
     def Preview(self):
-        AddedSquareBoarder("DSC_0647.jpg",(self.backgroundColorRValue,self.backgroundColorGValue,self.backgroundColorBValue)).show("Sample 1")
-        AddedSquareBoarder("DSC_9871.jpg",(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue)).show("Sample 2")
+        AddedSquareBoarder("DSC_0647.jpg",(self.backgroundColorRValue,self.backgroundColorGValue,self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue),self.textSize, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).show("Sample 1")
+        AddedSquareBoarder("DSC_9871.jpg",(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue), self.textSize, (self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).show("Sample 2")
     def Start(self):
         for image in os.listdir(self.sourceDirectory):
-            AddedSquareBoarder(str(Path(self.sourceDirectory)/Path(image)),(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue)).save(str(Path(self.targetDirectory)/Path("framed"+image)))
+            AddedSquareBoarder(str(Path(self.sourceDirectory)/Path(image)),(self.backgroundColorRValue, self.backgroundColorGValue, self.backgroundColorBValue), (self.textColorRValue, self.textColorGValue,self.textColorBValue),self.textSize,(self.imageWidthRatioValue/self.imageHeightRatioValue), self.displayTopLeft,self.displayTopRight,self.displayBottomLeft,self.displayBottomRight).save(str(Path(self.targetDirectory)/Path("framed"+image)))
 
+    def SetDisplayTopLeft(self, checked):
+        self.displayTopLeft = checked
+    def SetDisplayTopRight(self, checked):
+        self.displayTopRight = checked
+    def SetDisplayBottomLeft(self, checked):
+        self.displayBottomLeft = checked
+    def SetDisplayBottomRight(self, checked):
+        self.displayBottomRight = checked
+    def SetBackgroundColorRValue(self, value):
+        self.backgroundColorRValue = int(value)
+    def SetBackgroundColorGValue(self, value):
+        self.backgroundColorGValue = int(value)
+    def SetBackgroundColorBValue(self, value):
+        self.backgroundColorBValue = int(value)
+    def SetTextColorRValue(self, value):
+        self.textColorRValue = int(value)
+    def SetTextColorGValue(self, value):
+        self.textColorGValue = int(value)
+    def SetTextColorBValue(self, value):
+        self.textColorBValue = int(value)
+    def SetImageWidthRatioValue(self, value):
+        self.imageWidthRatioValue = value
+    def SetImageHeightRatioValue(self, value):
+        self.imageHeightRatioValue = value
+    def SetTextSize(self, value):
+        self.textSize = value
 app = QApplication([])
 
 # Create a Qt widget, which will be our window.
 window = MainWindow()
+window.setWindowTitle("Image Frame Generator")
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
